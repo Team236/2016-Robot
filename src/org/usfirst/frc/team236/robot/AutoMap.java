236e0888481163f9719a4a7bc4baae575a92832e
Sam Fuchs <samcfuchs@gmail.com>
2016-01-23 10:39:11 -0500

    Added AutoMap to store autonomous constants

---
+++ src/org/usfirst/frc/team236/robot/AutoMap.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team236.robot;
+
+import org.usfirst.frc.team236.robot.motionProfile.ProfileParameters;
+
+public class AutoMap {
+	public static ProfileParameters test = new ProfileParameters(10, 5, 4, 2);
+}
