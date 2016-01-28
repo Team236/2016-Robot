6dc5d5d46bda52b8e0fc5628c219839e10f7fd7b
Sam Fuchs <samcfuchs@gmail.com>
2016-01-28 10:10:39 -0500

    Initial commit

--- src/org/usfirst/frc/team236/robot/OI.java
+++ src/org/usfirst/frc/team236/robot/OI.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team236.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
