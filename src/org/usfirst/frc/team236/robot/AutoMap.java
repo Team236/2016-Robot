ec48e35a1abb746e1814cb9a3decbb34265b163d
Sam Fuchs <samcfuchs@gmail.com>
2016-01-30 14:03:43 -0500

    Added new auto mode params

--- src/org/usfirst/frc/team236/robot/AutoMap.java
+++ src/org/usfirst/frc/team236/robot/AutoMap.java
@@ -4,4 +4,10 @@ import org.usfirst.frc.team236.robot.motionProfile.ProfileParameters;
 
 public class AutoMap {
 	public static ProfileParameters test = new ProfileParameters(10, 5, 4, 2);
+	/*
+	 * Drives from the midfield to the position aligned with the left goal by
+	 * going under the low bar. Displacement is correct, the other values are
+	 * placeholders
+	 */
+	public static ProfileParameters toShoot = new ProfileParameters(6.19, 5, 4, 2);
 }
