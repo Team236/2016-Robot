b1acbc7fc8e1d9626b9339e0a47d73e7e168fbde
Sam Fuchs <samcfuchs@gmail.com>
2016-02-09 19:09:57 -0500

    Added negatives to joystick axes because joysticks don't read correctly

--- src/org/usfirst/frc/team236/robot/commands/DriveWithJoysticks.java
+++ src/org/usfirst/frc/team236/robot/commands/DriveWithJoysticks.java
@@ -21,8 +21,8 @@ public class DriveWithJoysticks extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.drive.setLeftSpeed(Robot.oi.leftStick.getY());
-		Robot.drive.setRightSpeed(Robot.oi.rightStick.getY());
+		Robot.drive.setLeftSpeed(-Robot.oi.leftStick.getY());
+		Robot.drive.setRightSpeed(-Robot.oi.rightStick.getY());
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
