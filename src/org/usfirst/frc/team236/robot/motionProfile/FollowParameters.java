d0d7eb88be37de10671ff5df234ca10ebe094277
Sam Fuchs <samcfuchs@gmail.com>
2016-02-11 22:23:17 -0500

    Added Follow params to help with creating profilefollowers

---
+++ src/org/usfirst/frc/team236/robot/motionProfile/FollowParameters.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team236.robot.motionProfile;
+
+public class FollowParameters {
+	public double kP, kV, kA;
+
+	public FollowParameters(double _kP, double _kV, double _kA) {
+		this.kP = _kP;
+		this.kV = _kV;
+		this.kA = _kA;
+	}
+}
