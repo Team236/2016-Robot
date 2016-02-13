1a44d20a7f3b095b6f168e3f6a3bae505556b077
Sam Fuchs <samcfuchs@gmail.com>
2016-02-13 09:32:38 -0500

    Added timeouts to commands

--- src/org/usfirst/frc/team236/robot/commands/Cock.java
+++ src/org/usfirst/frc/team236/robot/commands/Cock.java
@@ -8,35 +8,38 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class Cock extends Command {
-	// TODO
-    public Cock() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.shooter);
-    }
+	private int i;
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	public Cock() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.shooter);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	Robot.shooter.setSpeed(1);
-    	Robot.shooter.setSol(-1); // Compresses the spring. Stops when limit switch is hit using Spark.
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		i = 0;
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		Robot.shooter.setSpeed(.5); // Retract spring
+		Robot.shooter.insertPin();
+		i++;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.shooter.setSpeed(0);
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return (i / 50) > 5; // Time out after 5 seconds
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.shooter.stop();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
