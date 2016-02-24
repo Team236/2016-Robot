9df14a544cede7cb9caa9ac164b1404708b4e0ad
Sam Fuchs <samcfuchs@gmail.com>
2016-02-23 23:16:19 -0500

    Added Shift Toggle command

---
+++ src/org/usfirst/frc/team236/robot/commands/ShiftToggle.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team236.robot.commands;
+
+import org.usfirst.frc.team236.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class ShiftToggle extends CommandGroup {
+
+	public ShiftToggle() {
+		// Add Commands here:
+		// e.g. addSequential(new Command1());
+		//      addSequential(new Command2());
+		// these will run in order.
+
+		// To run multiple commands at the same time,
+		// use addParallel()
+		// e.g. addParallel(new Command1());
+		//      addSequential(new Command2());
+		// Command1 and Command2 will run in parallel.
+
+		// A command group will require all of the subsystems that each member
+		// would require.
+		// e.g. if Command1 requires chassis, and Command2 requires arm,
+		// a CommandGroup containing them would require both the chassis and the
+		// arm.
+		if (Robot.drive.gear == 1) {
+
+		} else if (Robot.drive.gear == 0) {
+
+		}
+	}
+}
