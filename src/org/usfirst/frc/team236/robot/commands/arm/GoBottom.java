182080564a5ee69e389a64248f59cb415ddab2b4
Sam Fuchs <samcfuchs@gmail.com>
2016-03-04 22:18:21 -0500

    Changed GoBottom end condition and PID gains

--- src/org/usfirst/frc/team236/robot/commands/arm/GoBottom.java
+++ src/org/usfirst/frc/team236/robot/commands/arm/GoBottom.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team236.robot.commands.arm;
 
 import org.usfirst.frc.team236.robot.Robot;
-import org.usfirst.frc.team236.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -9,10 +8,6 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class GoBottom extends Command {
-
-	private static final double kP_down = RobotMap.ArmMap.downPID.kP;
-	private static final double kI_down = RobotMap.ArmMap.downPID.kI;
-	private static final double kD_down = RobotMap.ArmMap.downPID.kD;
 
 	public GoBottom() {
 		// Use requires() here to declare subsystem dependencies
@@ -22,7 +17,8 @@ public class GoBottom extends Command {
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
-		Robot.arm.getPIDController().setPID(kP_down, kI_down, kD_down);
+		Robot.arm.enable();
+		Robot.arm.getPIDController().setPID(.02, 0, 0);
 		Robot.arm.setSetpoint(-10.5);
 	}
 
@@ -32,11 +28,12 @@ public class GoBottom extends Command {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return Robot.arm.bottomLimit.get() || Robot.arm.onTarget();
+		return Robot.arm.bottomLimit.get();
 	}
 
 	// Called once after isFinished returns true
 	protected void end() {
+		Robot.arm.disable();
 		Robot.arm.stop();
 		Robot.arm.zeroEncoder();
 	}
@@ -44,5 +41,6 @@ public class GoBottom extends Command {
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
+		end();
 	}
 }
