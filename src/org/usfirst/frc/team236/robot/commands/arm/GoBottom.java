076a9377c9d2c1217367c9903ace98b30f699a9d
Sam Fuchs <samcfuchs@gmail.com>
2016-03-14 20:01:16 -0400

    Updated references to use RobotMap in arm commands

--- src/org/usfirst/frc/team236/robot/commands/arm/GoBottom.java
+++ src/org/usfirst/frc/team236/robot/commands/arm/GoBottom.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team236.robot.commands.arm;
 
 import org.usfirst.frc.team236.robot.Robot;
+import org.usfirst.frc.team236.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -8,36 +9,36 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class GoBottom extends Command {
-
+	
 	public GoBottom() {
 		// Use requires() here to declare subsystem dependencies
 		// eg. requires(chassis);
 		requires(Robot.arm);
 	}
-
+	
 	// Called just before this Command runs the first time
 	protected void initialize() {
 		Robot.arm.enable();
 		Robot.arm.getPIDController().setPID(.02, 0, 0);
-		Robot.arm.setSetpoint(-10.5);
+		Robot.arm.setSetpoint(RobotMap.ArmMap.MIN_ANGLE);
 	}
-
+	
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
 	}
-
+	
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return Robot.arm.bottomLimit.get();
+		return Robot.arm.bottomLimit.get() || Robot.arm.onTarget();
 	}
-
+	
 	// Called once after isFinished returns true
 	protected void end() {
 		Robot.arm.disable();
 		Robot.arm.stop();
 		Robot.arm.zeroEncoder();
 	}
-
+	
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
